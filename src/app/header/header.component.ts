f63998f59043b03103cebb873c697538d375de35
Juan Felipe Rada <j.f.r.a1997@gmail.com>
2022-10-04 15:31:19 -0300

    fix: :fire: Fix style of change theme button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: [ './header.component.scss' ],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: [ './header.component.scss' ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent
 {
-    public selectMode: boolean = true;
+  public selectMode!: boolean;
 
-    public changeTheme()
-    {
-        document.body.classList.toggle('dark-theme');
-    }
+  public changeTheme()
+  {
+    document.body.classList.toggle('dark-theme');
+    this.selectMode = !this.selectMode;
+  }
 
 }
